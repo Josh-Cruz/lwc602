391e0f404547238a854b42ff7db0f0437964190c
Josh Cruz <joshcruz@Joshs-MacBook-Pro.local>
2020-04-28 13:39:13 -0700

    Exercise 2-10 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,10 +1,14 @@
 import { LightningElement, track, wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import { fireEvent } from 'c/pubsub';
+import { CurrentPageReference } from 'lightning/navigation';
 
 export default class StudentBrowser extends LightningElement {
 
     selectedDeliveryId = '';
 selectedInstructorId = '';
+
+@wire(CurrentPageReference) pageRef;
 
 @wire(getStudents, { instructorId: '$selectedInstructorId',
 courseDeliveryId: '$selectedDeliveryId' })
@@ -15,5 +19,12 @@ handleFilterChange(event){
     this.selectedInstructorId = event.detail.instructorId;
     }
 
+    handleStudentSelected(event){
+        const studentId = event.detail.studentId;
+        this.updateSelectedStudent(studentId);
+        }
+        updateSelectedStudent(studentId){
+            fireEvent(this.pageRef, 'studentChange', { studentId });
+            }
 
 }
